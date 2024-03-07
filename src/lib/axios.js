e5666b0865b5e148384ccfbcc16c87cfaa4ed12a
Renisson Silva <renissonx@gmail.com>
2024-03-07 00:29:36 -0300

    Alterando url do backend para o do Heroku

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://oraculo-mwmg.onrender.com'
+    baseURL: 'https://oraculo-app-d25156cd4a79.herokuapp.com/'
+    // baseURL: 'https://oraculo-mwmg.onrender.com'
     // baseURL: 'http://10.0.2.2:3333'
 });
