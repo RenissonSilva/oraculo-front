7750e6a6787a8c04ec06b73cd2ba95a8f838d274
Renisson Silva <renissonx@gmail.com>
2024-03-08 01:12:46 -0300

    Alterando cores e removendo splash por conta da play store

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,10 @@
 import { View } from "react-native";
-import { DotsThreeVertical } from 'phosphor-react-native';
-
-
 import Logo from '../assets/logo.svg'
 
 export function Header() {
     return (
         <View className="w-full flex-row items-center justify-between mb-4">
             <Logo />
-
-            {/* <DotsThreeVertical color="#fff" size={32} /> */}
         </View>
     )
 }
